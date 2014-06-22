90b5753205277c90fac674c717da1bcc07b25d06
John Nieves <karn09@gmail.com>
2014-06-22 16:57:10 -0400

    fixed instagram_data.code check

--- public/main.js
+++ public/main.js
@@ -1,11 +1,10 @@
 $(function() {
-    var token = window.location.hash.replace("#access_token", "");
+    var token = window.location.hash.replace("#access_token=", "");
+    console.log(token);
     var access_token = {
         access_token: token
     };
     // make API call
-    tag = "sneakers";
-
     function getImages(tag, access_token) {
         var url,
             count = 12; // return at most 12 images at any given time
@@ -18,13 +17,13 @@ $(function() {
             success: loadData
         });
     }
-
+    // load images from instagram 
     function loadData(instagram_data) {
         var photos, photo, key, new_image;
         console.log(instagram_data);
-        if (instagram_data.meta === 200) {
+        if (instagram_data.meta.code === 200) {
             photos = instagram_data.data;
-            console.log(photos);
+
             for (i in photos) {
                 photo = photos[i];
                 new_image = $('<img src="' + photo.images.standard_resolution.url + '">');
